196b9723bdaef43020dfedf4847004fb6c7dd14f
Babaneh <travisneh1996@gmail.com>
2024-10-08 07:31:20 +0100

    Lesson 12: Using the shadow DOM

--- tooltip.js
+++ tooltip.js
@@ -4,27 +4,34 @@ class Tooltip extends HTMLElement {
   constructor() {
     super();
     this._tooltipContainer;
+    this._tooltipText = 'Some dummy text.';
+    this.attachShadow({ mode: 'open' });
   }
 
   connectedCallback() {
-    this._tooltipText = this.getAttribute('text');
+    if (this.hasAttribute('text')) {
+      this._tooltipText = this.getAttribute('text');
+    }
     const tooltipIcon = document.createElement('span');
-    tooltipIcon.textContent = '(?)';
+    tooltipIcon.textContent = ' (?)';
     tooltipIcon.addEventListener('mouseover', this._showTooltip.bind(this));
     tooltipIcon.addEventListener('mouseleave', this._hideTooltip.bind(this));
-    this.appendChild(tooltipIcon);
+    this.shadowRoot.appendChild(tooltipIcon);
+    this.style.position = 'relative';
   }
 
   _showTooltip() {
     this._tooltipContainer = document.createElement('div');
-    this._tooltipContainer.textContent = 'This is the tooltiptext!.';
+    this._tooltipContainer.textContent = this._tooltipText;
     this._tooltipContainer.style.backgroundColor = 'black';
     this._tooltipContainer.style.color = 'white';
-    this.appendChild(this._tooltipContainer);
+    this._tooltipContainer.style.position = 'absolute';
+    this._tooltipContainer.style.zIndex = '10';
+    this.shadowRoot.appendChild(this._tooltipContainer);
   }
 
   _hideTooltip() {
-    this.removeChild(this._tooltipContainer);
+    this.shadowRoot.removeChild(this._tooltipContainer);
   }
 
 }
